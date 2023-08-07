51aa049e61ad8769fac8901d88d39a1bc3d68e27
contehboy <133304258+contehboy@users.noreply.github.com>
2023-08-07 06:40:59 -0500

    initial js commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("searchForm");
+const cityInput = document.getElementById("cityInput");
+const historyList = document.getElementById("historyList");
+const currentWeatherDiv = document.getElementById("currentWeather");
+const forecastCardsDiv = document.getElementById("forecastCards");
